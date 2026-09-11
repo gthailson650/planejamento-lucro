05abbd16a1173878f6f22638c88263af649f78e9
gthailson650 <gthailson650@gmail.com>
2026-09-11 19:45:57 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "planejamento-lucro-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ARQUIVOS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(chaves => {
+      return Promise.all(
+        chaves
+          .filter(chave => chave !== CACHE_NAME)
+          .map(chave => caches.delete(chave))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resposta => {
+      return resposta || fetch(event.request);
+    })
+  );
+});
