41c9bbb73ff5b563c9c3397fe794324fd2ad71a6
gthailson650 <gthailson650@gmail.com>
2026-09-11 21:59:21 -0300

    Simplify catch block for cache fallback

--- sw.js
+++ sw.js
@@ -45,9 +45,7 @@ self.addEventListener("fetch", event => {
 
           return resposta;
         })
-        .catch(() => {
-          return caches.match("./index.html");
-        })
+        .catch(() => caches.match("./index.html"))
     );
 
     return;
